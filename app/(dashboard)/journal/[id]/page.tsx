25c72c1f2a78037ec6db7ee1dd5235808f19e498
Hussain Ali <huali235@gmail.com>
2024-10-28 11:27:06 -0400

    analysis sidebar ui

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -18,10 +18,38 @@ const getEntry = async (id) => {
 
 const EntryPage = async ({ params }) => {
   const entry = await getEntry(params.id)
+  const analysisData = [
+    { name: 'Summary', value: '' },
+    { name: 'Subject', value: '' },
+    { name: 'Mood', value: '' },
+    { name: 'Negative', value: 'false' },
+  ]
   return (
-    <div>
-      <Editor entry={entry} />
-    </div>
+    <>
+      <div className="flex flex-col lg:flex-row lg:space-x-6 p-6">
+        <div className="flex-1">
+          <Editor entry={entry} />
+        </div>
+        <div className="w-full lg:w-1/3 bg-gray-100 p-6 rounded-lg shadow-md">
+          <h2 className="text-xl font-semibold mb-4">Analysis</h2>
+          <div>
+            <ul>
+              {analysisData.map((item) => (
+                <li
+                  key={item.name}
+                  className="flex items-center justify-between py-2 border-b-2"
+                >
+                  <span className="font-semibold text-gray-700">
+                    {item.name}:
+                  </span>
+                  <span className="text-gray-500">{item.value}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
