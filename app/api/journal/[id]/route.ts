e564a7938a8ee3d994f9bf21ba950520187d5b55
Hussain Ali <huali235@gmail.com>
2024-10-29 15:39:32 -0400

    push data to database

--- app/api/journal/[id]/route.ts
+++ app/api/journal/[id]/route.ts
@@ -1,3 +1,4 @@
+import { analyze } from '@/utils/ai'
 import { getUserByClerkID } from '@/utils/auth'
 import { prisma } from '@/utils/db'
 import { NextResponse } from 'next/server'
@@ -15,6 +16,18 @@ export const PATCH = async (request, { params }) => {
     data: {
       content,
     },
+  })
+
+  const analysis = await analyze(updatedEntry.content)
+  await prisma.analysis.upsert({
+    where: {
+      entryId: updatedEntry.id,
+    },
+    create: {
+      entryId: updatedEntry.id,
+      ...analysis,
+    },
+    update: analysis,
   })
 
   return NextResponse.json({ data: updatedEntry })
