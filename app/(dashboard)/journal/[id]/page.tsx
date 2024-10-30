5045e5d369028849b4426db49920d95f13d5a978
Hussain Ali <huali235@gmail.com>
2024-10-30 10:31:02 -0400

    refractor code to Editor

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -21,44 +21,12 @@ const getEntry = async (id) => {
 
 const EntryPage = async ({ params }) => {
   const entry = await getEntry(params.id)
-  const { summary, subject, mood, negative, color } = entry?.analysis
-  const analysisData = [
-    { name: 'Summary', value: summary },
-    { name: 'Subject', value: subject },
-    { name: 'Mood', value: mood },
-    { name: 'Negative', value: negative ? 'True' : 'False' },
-  ]
   return (
-    <>
-      <div className="flex flex-col lg:flex-row lg:space-x-6 p-6">
-        <div className="flex-1">
-          <Editor entry={entry} />
-        </div>
-        <div className="w-full lg:w-1/3 bg-gray-100 p-6 rounded-lg shadow-md">
-          <h2
-            className="text-xl font-semibold mb-4"
-            style={{ backgroundColor: color }}
-          >
-            Analysis
-          </h2>
-          <div>
-            <ul>
-              {analysisData.map((item) => (
-                <li
-                  key={item.name}
-                  className="flex items-center justify-between py-2 border-b-2"
-                >
-                  <span className="font-semibold text-gray-700">
-                    {item.name}:
-                  </span>
-                  <span className="text-gray-500">{item.value}</span>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
+    <div className="flex flex-col lg:flex-row lg:space-x-6 p-6">
+      <div className="flex-1">
+        <Editor entry={entry} />
       </div>
-    </>
+    </div>
   )
 }
 
