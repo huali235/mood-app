01be635d19a27f711bc7fb926ed7d4e39a13de63
Hussain Ali <huali235@gmail.com>
2024-10-30 10:31:46 -0400

    display analysis when hitting submit button

--- app/api/journal/[id]/route.ts
+++ app/api/journal/[id]/route.ts
@@ -19,7 +19,7 @@ export const PATCH = async (request, { params }) => {
   })
 
   const analysis = await analyze(updatedEntry.content)
-  await prisma.analysis.upsert({
+  const updated = await prisma.analysis.upsert({
     where: {
       entryId: updatedEntry.id,
     },
@@ -30,5 +30,5 @@ export const PATCH = async (request, { params }) => {
     update: analysis,
   })
 
-  return NextResponse.json({ data: updatedEntry })
+  return NextResponse.json({ data: { ...updatedEntry, analysis: updated } })
 }
--- components/Editor.tsx
+++ components/Editor.tsx
@@ -6,6 +6,14 @@ import { useState } from 'react'
 const Editor = ({ entry }) => {
   const [content, setContent] = useState(entry.content)
   const [isSaving, setIsSaving] = useState(false)
+  const [analysis, setAnalysis] = useState(entry.analysis)
+  const { summary, subject, mood, negative, color } = analysis
+  const analysisData = [
+    { name: 'Summary', value: summary },
+    { name: 'Subject', value: subject },
+    { name: 'Mood', value: mood },
+    { name: 'Negative', value: negative ? 'True' : 'False' },
+  ]
 
   const handleChange = (e) => {
     setContent(e.target.value)
@@ -14,7 +22,9 @@ const Editor = ({ entry }) => {
   const handleSave = async () => {
     setIsSaving(true)
     try {
-      await updateEntry(entry.id, content)
+      const data = await updateEntry(entry.id, content)
+      setAnalysis(data.analysis)
+      console.log(analysis)
     } catch (error) {
       console.error('Failed to save content', error)
     } finally {
@@ -22,21 +32,46 @@ const Editor = ({ entry }) => {
     }
   }
   return (
-    <div className="max-w-2xl mx-auto p-6 bg-white shadow-md rounded-lg">
-      <h1 className="text-2xl font-semibold mb-4">Journal Entry</h1>
-      <textarea
-        className="w-full h-64 p-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-        value={content}
-        onChange={handleChange}
-      />
-      <div className="mt-4 flex justify-end">
-        <button
-          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors duration-300"
-          onClick={handleSave}
-          disabled={isSaving}
+    <div className="flex flex-col lg:flex-row lg:space-x-6 p-6">
+      <div className="flex-1 max-w-2xl mx-auto p-6 bg-white shadow-md rounded-lg">
+        <h1 className="text-2xl font-semibold mb-4">Journal Entry</h1>
+        <textarea
+          className="w-full h-64 p-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          value={content}
+          onChange={handleChange}
+        />
+        <div className="mt-4 flex justify-end">
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors duration-300"
+            onClick={handleSave}
+            disabled={isSaving}
+          >
+            {isSaving ? 'Saving...' : 'Save'}
+          </button>
+        </div>
+      </div>
+      <div className="w-full lg:w-1/3 bg-gray-100 p-6 rounded-lg shadow-md">
+        <h2
+          className="text-xl font-semibold mb-4"
+          style={{ backgroundColor: color }}
         >
-          {isSaving ? 'Saving...' : 'Save'}
-        </button>
+          Analysis
+        </h2>
+        <div>
+          <ul>
+            {analysisData.map((item) => (
+              <li
+                key={item.name}
+                className="flex items-center justify-between py-2 border-b-2"
+              >
+                <span className="font-semibold text-gray-700">
+                  {item.name}:
+                </span>
+                <span className="text-gray-500">{item.value}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   )
